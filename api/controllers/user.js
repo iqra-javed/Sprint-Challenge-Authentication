d1df9139cf9925435016d216ea5e03dee9a3f093
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-18 20:49:56 -0500

    create & save user with encrypted password

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,14 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create({ username, password })
+    .then(user => {
+      const token = jwt.generateToken({ username: user.username });
+      res.status(201).json({ token });
+    })
+    .catch(error => {
+      res.status(500).json({ error })
+    })
 };
 
 module.exports = {
